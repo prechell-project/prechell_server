8d9401b94d14d3ceeb375bd260fcd376febe0af4
pincle0919 <157594257+pincle0919@users.noreply.github.com>
2024-05-28 19:31:19 +0900

    초기세팅

---
+++ src/main/java/com/api/prechell/PrechellApplication.java
@@ -0,0 +1,13 @@
+package com.api.prechell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrechellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrechellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/prechell/PrechellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.prechell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrechellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
